caaa06e0600f848f1b281b11e6a01c1da57d4c52
Joe Centeno <centeno.joe@gmail.com>
2024-01-05 13:30:23 -0800

    Added Chat Controller for POC

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -3,11 +3,17 @@ using CCIPICL_ChatAssistant.Models;
 
 namespace CCIPICL_ChatAssistant.Controllers
 {
-    public class ChatController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpPost]
+        public ActionResult<ChatMessage> Post([FromBody] ChatMessage message)
         {
-            return View();
+            // Here I will do the GPT model API call and get a response than send it back to the website
+             // For now I am gonna return a test reponse for making sure I set up the API correctly
+            message.BotResponse = "You said: " + message.UserMessage;
+            return message;
         }
     }
 }
