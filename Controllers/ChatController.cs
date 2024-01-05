75af98acee7557fd25b0f94cfdc4d495edce4e06
Joe Centeno <centeno.joe@gmail.com>
2024-01-05 13:26:19 -0800

    Created chat model and did init clean up of project

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CCIPICL_ChatAssistant.Models;
+
+namespace CCIPICL_ChatAssistant.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace CCIPICL_ChatAssistant.Models
+{
+   public class ChatMessage
+    {
+       public string SessionId { get; set; }
+       public string UserMessage { get; set; }
+       public string BotResponse { get; set; }
+    }
+}
